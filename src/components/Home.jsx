e758325f044c053289028f0d33c41fbef8db36c9
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-07 17:40:24 -0300

    add Header and Main components to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,11 @@
+import Header from './Header'
+import Main from './Main'
+
 export default function Home() {
-  return "Home"
+  return (
+    <div class="bg-white">
+      <Header />
+      <Main />
+    </div>
+  )
 }
