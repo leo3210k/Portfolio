3ad1eb0e5919efea8f4a0a7f4d49d26fbc268fd6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-10 16:35:39 -0300

    add underline on contact in main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,7 +22,7 @@ export default function Main() {
             <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Resume
             </a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Contact</a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900 underline underline-offset-1">Contact</a>
           </div>
         </div>
       </div>
