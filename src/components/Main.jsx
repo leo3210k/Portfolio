4e9fb7ea864d68b1d1d379647f039d27f94b4ace
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-10 17:54:53 -0300

    add profile picture to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,19 @@
+import Image from 'next/image'
+import profile from '../../public/images/Leonardo_image.jpg'
+
 export default function Main() {
   return (
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
       </div>
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div class="mx-44 flex gap-x-20 items-center py-32 sm:py-48 lg:py-24">
         {/* <div class="hidden sm:mb-8 sm:flex sm:justify-center">
           <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
           </div>
         </div> */}
+        <Image src={profile} className="w-6/12" alt="profile"/>
         <div class="flex flex-col items-start">
           <h1 class="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-6xl">
             Turning Vision Into Reality With Code And Design.
