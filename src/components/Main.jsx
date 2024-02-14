c5b855e1ae3cbd31f34c75b6b9aa8fbe2695c8a6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-14 13:11:53 -0300

    fix bottom figure position

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,7 +25,7 @@ export default function Main() {
           </div>
         </div>
       </div>
-      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+      <div class="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
         <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
       </div>
     </div>
