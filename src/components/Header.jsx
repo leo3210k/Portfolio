54ac8485499631f04e00b085e70894c82fc0e54f
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-14 17:40:36 -0300

    add customlink and animations to header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,61 +4,81 @@ import github from '../../public/images/svgs/github.svg'
 import instagram from '../../public/images/svgs/instagram.svg'
 import linkedin from '../../public/images/svgs/linkedin.svg'
 import twitter from '../../public/images/svgs/twitter.svg'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const CustomLink = ({ href, title, className="" }) => {
+  const router = useRouter();
+  return(
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span className={`
+      h-[1px] inline-block bg-black
+      absolute left-0 -bottom-0.5
+      group-hover:w-full transition-[width] ease duration-300
+      underline-offset-4
+      ${router.asPath === href ? "w-full" : "w-0"}
+      `}
+      >&nbsp;</span>
+    </Link>
+  )
+}
 
 export default function Header() {
   return (
-    <header class="absolute inset-x-0 top-0 z-50">
-      <nav class="flex items-center justify-between lg:px-8" aria-label="Global">
-        <div class="flex lg:flex-1">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav className="flex items-center justify-between lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
             <Image src={logo} className="w-6/12 h-auto" alt="logo"/>
           </a>
         </div>
-        <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-            <span class="sr-only">Open main menu</span>
-            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+        <div className="flex lg:hidden">
+          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span className="sr-only">Open main menu</span>
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
         </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-          <a href="#" class="text-lg font-normal leading-6 text-gray-900">Home</a>
-          <a href="#" class="text-lg font-normal leading-6 text-gray-900">About</a>
-          <a href="#" class="text-lg font-normal leading-6 text-gray-900">Projects</a>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <CustomLink href="/" title="Home" />
+          <CustomLink href="/about" title="About" />
+          <CustomLink href="/projects" title="Projects" />
         </div>
-        <div class="hidden gap-x-8 lg:flex lg:flex-1 lg:justify-end">
+        <div className="hidden gap-x-8 lg:flex lg:flex-1 lg:justify-end">
           <Image src={github} className="w-6 h-auto" alt="logo"/>
           <Image src={linkedin} className="w-6 h-auto" alt="logo"/>
           <Image src={instagram} className="w-6 h-auto" alt="logo"/>
           <Image src={twitter} className="w-6 h-auto" alt="logo"/>
         </div>
       </nav>
-      <div class="lg:hidden" role="dialog" aria-modal="true">
-        <div class="fixed inset-0 z-50"></div>
-        <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
-              <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
+      <div className="lg:hidden" role="dialog" aria-modal="true">
+        <div className="fixed inset-0 z-50"></div>
+        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
             </a>
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-              <span class="sr-only">Close menu</span>
-              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
+              <span className="sr-only">Close menu</span>
+              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
-          <div class="mt-6 flow-root">
-            <div class="-my-6 divide-y divide-gray-500/10">
-              <div class="space-y-2 py-6">
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a>
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a>
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Projects</a>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Projects</a>
               </div>
-              <div class="py-6">
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+              <div className="py-6">
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
               </div>
             </div>
           </div>
