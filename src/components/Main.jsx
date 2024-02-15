9468fda4bc3c24b69e7e661fdf8ea13f917f194b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-15 08:47:44 -0300

    add height to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 
 export default function Main({ children }) {
   return (
-    <div class="relative isolate px-6 pt-6 lg:px-8">
+    <div class="min-h-svh relative isolate px-6 pt-6 lg:px-8">
       <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
       </div>
       {children}
-      <div class="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
+      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
         <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
       </div>
     </div>
