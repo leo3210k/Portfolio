aec5616c7e90a579cacea0fe720b534dd9b6951c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-15 16:49:54 -0300

    add bow shadow to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        '3xl': '7px 10px 0 5px rgba(0, 0, 0, 0.8)',
+      }
     },
   },
   plugins: [],
