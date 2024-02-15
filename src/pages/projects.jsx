42d1e630849cabc6a222cdd8a32501acb0c0ae4a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-15 08:49:41 -0300

    add Header and Main to projects page

---
+++ src/pages/projects.jsx
@@ -0,0 +1,13 @@
+import '../app/globals.css'
+
+import Header from '@/components/Header'
+import Main from '@/components/Main'
+
+export default function Projects() {
+  return (
+    <div className="text-center bg-white">
+      <Header />
+      <Main />
+    </div>
+  )
+}
