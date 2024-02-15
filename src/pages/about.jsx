3c37bf7b00a0eb70cbe5c337424869ab401b4b82
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-15 16:52:22 -0300

    add text size to informations

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,31 +11,27 @@ export default function About() {
     <div className="text-center bg-white">
       <Header />
       <Main>
-        <div class="px-24 flex justify-around items-center pt-40">
+        <div class="px-20 flex justify-around items-center pt-40">
           <div class="flex flex-col justify-center items-center">
             <h1 class="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-8xl">
               Passion Fuels Purpose!
             </h1>
-            <div className="mt-10 flex">
-              <div className="w-5/12">
+            <div className="mt-10 flex justify-around">
+              <div className="w-4/12">
                 <h2 class="text-xl text-left font-medium leading-8 text-gray-600">BIOGRAPHY</h2>
-                <p class="mt-6 text-lg text-left leading-8 text-gray-600">
+                <p class="mt-4 text-base text-left leading-6 text-gray-600">
                   Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                   and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and 
                   innovative ways to bring my clients' visions to life.
                 </p>
-                <p class="text-lg text-left leading-8 text-gray-600">
+                <p class="mt-4 text-base text-left leading-6 text-gray-600">
                   I believe that design is about more than just making things look pretty – it's about solving problems and creating 
                   intuitive, enjoyable experiences for users.
                 </p>
-                <p class="text-lg text-left leading-8 text-gray-600">
+                <p class="mt-4 text-base text-left leading-6 text-gray-600">
                   Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence 
                   and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and 
                   passion to your next project.
-                </p>
-                <p class="text-lg text-left leading-8 text-gray-600">
-                  As a skilled front-end developer, I am dedicated to turning ideas into innovative web applications. 
-                  Explore my latest projects, showcasing my expertise in React.js and web development.
                 </p>
               </div>
               <div className="w-3/12 h-auto p-7 rounded-2xl border-x-2 shadow-3xl">
@@ -43,7 +39,7 @@ export default function About() {
               </div>
               <div class="py-24 sm:py-32">
                 <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                  <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-1">
+                  <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center">
                     <div class="mx-auto flex max-w-xs flex-col gap-y-4">
                       <dt class="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
                       <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">44 million</dd>
