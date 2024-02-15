f0c01f029f6a2d22c2b46e388ccd6777849e372c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-15 17:36:35 -0300

    remove not utilized code

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,58 +11,49 @@ export default function About() {
     <div className="text-center bg-white">
       <Header />
       <Main>
-        <div class="px-10 flex justify-around items-center pt-40">
-          <div class="flex flex-col justify-center items-center">
-            <h1 class="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-8xl">
-              Passion Fuels Purpose!
-            </h1>
-            <div className="mt-20 flex justify-around">
-              <div className="w-4/12">
-                <h2 class="text-xl text-left font-medium leading-8 text-gray-600">BIOGRAPHY</h2>
-                <p class="mt-4 text-base text-left leading-6 text-gray-600">
-                  Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-                  and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and 
-                  innovative ways to bring my clients' visions to life.
-                </p>
-                <p class="mt-4 text-base text-left leading-6 text-gray-600">
-                  I believe that design is about more than just making things look pretty – it's about solving problems and creating 
-                  intuitive, enjoyable experiences for users.
-                </p>
-                <p class="mt-4 text-base text-left leading-6 text-gray-600">
-                  Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence 
-                  and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and 
-                  passion to your next project.
-                </p>
-              </div>
-              <div className="p-7 rounded-2xl border-2 shadow-3xl">
-                <Image src={profile} className="rounded-2xl bg-slate-950" alt="profile"/>
-              </div>
-              <div>
-                <div class="h-full mx-auto max-w-7xl px-6 lg:px-8">
-                  <dl class="h-full flex flex-col justify-between gap-x-8 text-center">
-                    <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                      <dt class="text-xl leading-7 text-gray-600">Satisfied Clients</dt>
-                      <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-7xl">44 +</dd>
-                    </div>
-                    <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                      <dt class="text-xl leading-7 text-gray-600">Projects Completed</dt>
-                      <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-7xl">50+</dd>
-                    </div>
-                    <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                      <dt class="text-xl leading-7 text-gray-600">Years Of Experience</dt>
-                      <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-7xl">4+</dd>
-                    </div>
-                  </dl>
-                </div>
+        <div class="px-10 pt-40 flex flex-col justify-center items-center">
+          <h1 class="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-8xl">
+            Passion Fuels Purpose!
+          </h1>
+          <div className="mt-20 flex justify-around">
+            <div className="w-4/12">
+              <h2 class="text-xl text-left font-medium leading-8 text-gray-600">BIOGRAPHY</h2>
+              <p class="mt-4 text-base text-left leading-6 text-gray-600">
+                Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and 
+                innovative ways to bring my clients' visions to life.
+              </p>
+              <p class="mt-4 text-base text-left leading-6 text-gray-600">
+                I believe that design is about more than just making things look pretty – it's about solving problems and creating 
+                intuitive, enjoyable experiences for users.
+              </p>
+              <p class="mt-4 text-base text-left leading-6 text-gray-600">
+                Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence 
+                and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and 
+                passion to your next project.
+              </p>
+            </div>
+            <div className="p-7 rounded-2xl border-2 shadow-3xl">
+              <Image src={profile} className="rounded-2xl bg-slate-950" alt="profile"/>
+            </div>
+            <div>
+              <div class="h-full mx-auto max-w-7xl px-6 lg:px-8">
+                <dl class="h-full flex flex-col justify-between gap-x-8 text-center">
+                  <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                    <dt class="text-xl leading-7 text-gray-600">Satisfied Clients</dt>
+                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-7xl">44 +</dd>
+                  </div>
+                  <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                    <dt class="text-xl leading-7 text-gray-600">Projects Completed</dt>
+                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-7xl">50+</dd>
+                  </div>
+                  <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                    <dt class="text-xl leading-7 text-gray-600">Years Of Experience</dt>
+                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-7xl">4+</dd>
+                  </div>
+                </dl>
               </div>
             </div>
-            {/* <div class="mt-10 flex items-center justify-center gap-x-6">
-              <Link href="/cv/cv.pdf" locale={false} target={"_blank"} class="flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Resume
-                <ExternalLink alt="profile"/>
-              </Link>
-              <Link href="mailto:leocoelho.pi@gmail.com" target={"_blank"} class="text-sm font-semibold leading-6 text-gray-900 underline underline-offset-1">Contact</Link>
-            </div> */}
           </div>
         </div>
       </Main>
