3c3b977c0569000c9616d77788d9d5ad4c82a301
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-15 09:11:17 -0300

    add title from about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,27 @@ export default function About() {
   return (
     <div className="text-center bg-white">
       <Header />
-      <Main />
+      <Main>
+        <div class="px-24 flex justify-around items-center pt-40">
+          {/* <Image src={profile} className="w-4/12 h-auto" alt="profile"/> */}
+          <div class="flex flex-col justify-center items-center">
+            <h1 class="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-8xl">
+              Passion Fuels Purpose!
+            </h1>
+            <p class="mt-6 text-lg text-left leading-8 text-gray-600">
+              As a skilled front-end developer, I am dedicated to turning ideas into innovative web applications. 
+              Explore my latest projects, showcasing my expertise in React.js and web development.
+            </p>
+            {/* <div class="mt-10 flex items-center justify-center gap-x-6">
+              <Link href="/cv/cv.pdf" locale={false} target={"_blank"} class="flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Resume
+                <ExternalLink alt="profile"/>
+              </Link>
+              <Link href="mailto:leocoelho.pi@gmail.com" target={"_blank"} class="text-sm font-semibold leading-6 text-gray-900 underline underline-offset-1">Contact</Link>
+            </div> */}
+          </div>
+        </div>
+      </Main>
     </div>
   )
 }
