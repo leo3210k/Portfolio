99b0372467e63804abc8be45ebf8929980a6abbc
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 15:57:59 -0300

    add project component to projects

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,15 +3,24 @@ import '../app/globals.css'
 
 import Header from '@/components/Header'
 import Main from '@/components/Main'
+import Project from '@/components/Project'
 
 export default function Projects() {
   return (
     <div className="text-center bg-white">
       <Header />
       <Main>
-        <div class="px-10 py-40 flex flex-col justify-center items-center">
+        <div class="px-10 pt-40 flex flex-col justify-center items-center">
           <AnimatedText text="Imagination Trumps Knowledge!"
-          className="px-40 text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-8xl"/>
+          className="px-40 pb-20 text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-8xl"/>
+          <div className="w-full">
+            <Project title="Podcasts" description="No auth needed podcasts web app" 
+            technologies="React, Typescript, Next.js, Styled Jsx, SWR."
+            github="link" demo="link"
+            image_1="/images/projects/oriental-food-desktop.jpeg"
+            image_2="/images/projects/oriental-food-mobile.jpeg"
+            />
+          </div>
         </div>
       </Main>
     </div>
