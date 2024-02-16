5ca46abdab7f9cadc27c83c8204b3b7c5f791b68
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 16:01:17 -0300

    add project description stylization

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,13 +8,13 @@ export default function Main({ title, description, technologies, image_1, image_
         <Image fill={true} src={`${image_1}`} className="object-contain"/>
         <Image fill={true} src={`${image_2}`} className="object-contain"/>
       </div>
-      <div>
-        <h1>{ title }</h1>
-        <h2>{ description }</h2>
-        <span>{ technologies }</span>
-        <div>
-          <Image src={icon} className="" alt="icon"/>
-          <Image src={icon} className="" alt="icon"/>
+      <div className="flex flex-col justify-center gap-y-1 text-left">
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">{ title }</h1>
+        <h2 className="font-medium text-gray-900">{ description }</h2>
+        <span className="mt-2 italic font-normal text-gray-600">{ technologies }</span>
+        <div className="mt-5 flex gap-x-6">
+          <Image src={icon} className="w-10" alt="icon"/>
+          <span className="flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline">Visit Project</span>
         </div>
       </div>
     </div>
