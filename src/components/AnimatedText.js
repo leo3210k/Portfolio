330ca6d99ade16ee1fa66989a9871a7443466eb2
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 14:03:04 -0300

    add motion to animated text

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,13 +1,46 @@
+import { motion } from "framer-motion"
+
 export default function AnimatedText({ text, className=""}) {
+  const quote = {
+    initial: {
+      opacity: 1,
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        staggerChildren: 0.08,
+      }
+    }
+  }
+
+  const singelWord = {
+    initial: {
+      opacity: 0,
+      y: 50,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1
+      }
+    }
+  }
+
   return (
-    <h1>
+    <motion.h1 className={`${className}`}
+    variants={quote}
+    initial="initial"
+    animate="animate">
       {
         text.split(" ").map((word, index) => 
-          <span key={word+'-'+index} className="inline-block">
+          <motion.span key={word+'-'+index} className="inline-block"
+          variants={singelWord}>
             {word}&nbsp;
-          </span>
+          </motion.span>
         )
       }
-    </h1>
+    </motion.h1>
   )
 }
