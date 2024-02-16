7ced51c87f44bae3531b2824c8f8387767f1a4a1
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 16:00:12 -0300

    add project images to project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
-import profile from '../../public/images/profile/Leonardo_about.png'
 import icon from '../../public/images/svgs/github.svg'
 
-export default function Main({ title, description, technologies }) {
+export default function Main({ title, description, technologies, image_1, image_2 }) {
   return (
-    <div class="flex content-center">
-      <div>
-        <Image src={profile} className="rounded-2xl bg-slate-950" alt="profile"/>
-        <Image src={profile} className="rounded-2xl bg-slate-950" alt="profile"/>
+    <div class="h-screen flex justify-evenly content-center">
+      <div className="relative w-1/12 h-">
+        <Image fill={true} src={`${image_1}`} className="object-contain"/>
+        <Image fill={true} src={`${image_2}`} className="object-contain"/>
       </div>
       <div>
         <h1>{ title }</h1>
