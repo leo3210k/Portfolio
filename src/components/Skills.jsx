bca4d31c988cafba832a305df5b2e8e7ac851f9b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 08:35:09 -0300

    add radial gradient to skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+export default function Skills() {
+  return (
+    <div className="mt-64 flex flex-col">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-8xl">Skills</h1>
+      <div className="w-full h-screen flex justify-center items-center rounded-full bg-circularLight">
+        <div className="flex justify-center items-center rounded-full font-semibold bg-black text-white
+        p-8 shadow-xl cursor-pointer">
+          Web
+        </div>
+      </div>
+    </div>
+  )
+}
