bab927f8209542112e2fa9b8a31c8c4e1cfdf52a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 08:36:37 -0300

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,39 @@
+import { motion } from "framer-motion"
+
 export default function Skills() {
+  const Skill = ({name, x, y}) => {
+
+    return(
+      <motion.div className="flex justify-center items-center rounded-full font-semibold bg-black text-white
+        p-8 shadow-xl cursor-pointer absolute"
+        whileHover={{scale: 1.05}}
+        initial={{x: 0, y: 0}}
+        animate={{x: x, y: y}}
+        transition={{duration: 1.5}}>
+          {name}
+      </motion.div>
+    )
+  }
+
   return (
     <div className="mt-64 flex flex-col">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-8xl">Skills</h1>
       <div className="w-full h-screen flex justify-center items-center rounded-full bg-circularLight">
-        <div className="flex justify-center items-center rounded-full font-semibold bg-black text-white
-        p-8 shadow-xl cursor-pointer">
+        <motion.div className="flex justify-center items-center rounded-full font-semibold bg-black text-white
+        p-8 shadow-xl cursor-pointer"
+        whileHover={{scale: 1.05}}>
           Web
-        </div>
+        </motion.div>
+        <Skill name="HTML" x="-20vw" y="2vw"/>
+        <Skill name="CSS" x="-5vw" y="-10vw"/>
+        <Skill name="Javascript" x="20vw" y="6vw"/>
+        <Skill name="ReactJS" x="0vw" y="12vw"/>
+        <Skill name="NextJS" x="-20vw" y="-15vw"/>
+        <Skill name="GatsbyJS" x="15vw" y="-12vw"/>
+        <Skill name="Web Design" x="32vw" y="-5vw"/>
+        <Skill name="Figma" x="0vw" y="-20vw"/>
+        <Skill name="Firebase" x="-25vw" y="18vw"/>
+        <Skill name="Tailwind CSS" x="18vw" y="18vw"/>
       </div>
     </div>
   )
