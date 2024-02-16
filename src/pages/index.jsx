a8e302582c12d9bbe0b932bc2c024ae0273051db
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 14:03:24 -0300

    add animated text component to index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import '../app/globals.css'
 
 import Header from '@/components/Header'
 import Main from '@/components/Main'
+import AnimatedText from '@/components/AnimatedText'
 
 import Image from 'next/image'
 import profile from '../../public/images/profile/Leonardo_1.png'
@@ -16,9 +17,8 @@ export default function Index() {
         <div class="px-24 flex justify-around items-center py-32 sm:py-48 lg:py-24">
           <Image src={profile} className="w-4/12 h-auto" alt="profile"/>
           <div class="w-5/12 flex flex-col items-start">
-            <h1 class="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Turning Vision Into Reality With Code And Design.
-            </h1>
+            <AnimatedText text="Turning Vision Into Reality With Code And Design."
+            className="text-4xl text-left font-bold tracking-tight text-gray-900 sm:text-7xl"/>
             <p class="mt-6 text-lg text-left leading-8 text-gray-600">
               As a skilled front-end developer, I am dedicated to turning ideas into innovative web applications. 
               Explore my latest projects, showcasing my expertise in React.js and web development.
