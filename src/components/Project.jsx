13e375fa74d26dd908c34b7b88ba732a49a2d1c6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-17 17:49:27 -0300

    add animation to projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import icon from '../../public/images/svgs/github.svg'
+import { motion } from 'framer-motion'
 
 export default function Main({ title, description, technologies, image_1, image_2, order=false }) {
   return (
-    <div class="h-screen flex justify-evenly content-center">
+    <motion.div class="h-screen flex justify-evenly content-center"
+    initial={{ opacity: 0, y: 50 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ delay: 1 }}
+    viewport={{ once: true }}>
       <div className={`relative self-center ${order ? 'order-last' : ''}`}>
         <Image src={`${image_1}`} width={800} height={800} className="rounded-xl shadow-2xl"/>
         <Image src={`${image_2}`} width={200} height={200} className="absolute rounded-xl top-32 -right-10 shadow-2xl"/>
@@ -17,6 +22,6 @@ export default function Main({ title, description, technologies, image_1, image_
           <span className="flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline">Visit Project</span>
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
