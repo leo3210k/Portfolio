824c0ab71261166fab665041994a55e3bae04e71
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 21:31:04 -0300

    add margin left to project description

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ export default function Main({ title, description, technologies, image_1, image_
         <Image src={`${image_1}`} width={800} height={800} className="rounded-xl shadow-2xl"/>
         <Image src={`${image_2}`} width={200} height={200} className="absolute rounded-xl top-32 -right-10 shadow-2xl"/>
       </div>
-      <div className="w-3/12 flex flex-col justify-center gap-y-1 text-left">
+      <div className="w-3/12 ml-32 flex flex-col justify-center content-center gap-y-1 text-left">
         <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">{ title }</h1>
         <h2 className="font-medium text-gray-900">{ description }</h2>
         <span className="mt-2 italic font-normal text-gray-600">{ technologies }</span>
