00afada07fbd7a27a64e1eb685b66803c047c381
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-16 21:03:22 -0300

    add width to project description

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,11 +4,11 @@ import icon from '../../public/images/svgs/github.svg'
 export default function Main({ title, description, technologies, image_1, image_2 }) {
   return (
     <div class="h-screen flex justify-evenly content-center">
-      <div className="relative w-1/12 h-">
-        <Image fill={true} src={`${image_1}`} className="object-contain"/>
-        <Image fill={true} src={`${image_2}`} className="object-contain"/>
+      <div className="self-center">
+        <Image src={`${image_1}`} width={800} height={800} className="shadow-2xl"/>
+        {/* <Image fill={true} src={`${image_2}`} className=""/> */}
       </div>
-      <div className="flex flex-col justify-center gap-y-1 text-left">
+      <div className="w-3/12 flex flex-col justify-center gap-y-1 text-left">
         <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">{ title }</h1>
         <h2 className="font-medium text-gray-900">{ description }</h2>
         <span className="mt-2 italic font-normal text-gray-600">{ technologies }</span>
