120ce26b1ba1cb5d70564f2cd40a80cece93ac41
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-18 23:49:32 -0300

    add useEffect and useState to transition effect timing

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -1,23 +1,38 @@
+import React from "react"
+import { useState } from "react";
 import { motion } from "framer-motion"
+import { useRouter } from "next/router"
 
 export default function TransitionEffect() {
- return (
-  <>
-    <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]"
-    initial={{x: "100%", width: "100%"}}
-    animate={{x :"0%", width: "0%"}}
-    transition={{duration: 0.8, ease: "easeInOut"}}
-    />
-    <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-indigo-600"
-    initial={{x: "100%", width: "100%"}}
-    animate={{x :"0%", width: "0%"}}
-    transition={{delay: 0.2, duration: 0.8, ease: "easeInOut"}}
-    />
-    <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-gray-900"
-    initial={{x: "100%", width: "100%"}}
-    animate={{x :"0%", width: "0%"}}
-    transition={{delay: 0.4, duration: 0.8, ease: "easeInOut"}}
-    />
-  </>
+  const router = useRouter()
+
+  React.useEffect(() => {
+    router.replace(router.route, undefined, { shallow: true });
+  }, [])
+
+  const [isThereQuery, setQuery] = useState(router.query.name);
+
+  return ( 
+    <>
+      {isThereQuery ? 
+        <>
+          <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]"
+          initial={{x: "100%", width: "100%"}}
+          animate={{x :"0%", width: "0%"}}
+          transition={{duration: 0.8, ease: "easeInOut"}}
+          />
+          <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-indigo-600"
+          initial={{x: "100%", width: "100%"}}
+          animate={{x :"0%", width: "0%"}}
+          transition={{delay: 0.2, duration: 0.8, ease: "easeInOut"}}
+          />
+          <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-gray-900"
+          initial={{x: "100%", width: "100%"}}
+          animate={{x :"0%", width: "0%"}}
+          transition={{delay: 0.4, duration: 0.8, ease: "easeInOut"}}
+          />
+        </>
+      : ""}
+    </>
   )
 }
