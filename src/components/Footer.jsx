756c33077a638214c7f50ce1668030393b607f7a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-20 23:40:31 -0300

    add ping animation to Heart

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import { Copyright, Heart } from "./Icons";
 export default function Footer({ children }) {
   return (
     <footer class="p-10 flex justify-around content-center border-t-2 border-gray-900">
-      <span className="flex">Built with Nextjs and &nbsp;<Heart className="w-6 h-auto text-red-700" alt="logo"/></span>
+      <span className="flex">
+        Built with Nextjs and &nbsp;&nbsp;
+        <Heart className="w-6 h-auto text-red-700 animate-ping-slow" alt="logo"/>
+      </span>
     </footer>
   )
 }
