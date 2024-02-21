5bc6348f63af1ee1f385fd804e8458e95746e33d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-20 23:23:44 -0300

    add footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
-export default function Footer() {
-  return "Footer"
+import { Copyright, Heart } from "./Icons";
+
+export default function Footer({ children }) {
+  return (
+    <footer class="p-10 flex justify-around content-center border-t-2 border-gray-900">
+      <span className="flex">Built with Nextjs and &nbsp;<Heart className="w-6 h-auto text-red-700" alt="logo"/></span>
+    </footer>
+  )
 }
