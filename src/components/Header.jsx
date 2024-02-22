098952c5c75d5e8b184730e72e517a01beb2b3ab
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-22 00:16:14 -0300

    add event click to menu icon and close menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from '../../public/images/logos/Logo_2.png'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Github, Instagram, Linkedin, Pinterest, Twitter } from './Icons'
+import { useState } from 'react'
 
 const CustomLink = ({ href, title, className="" }) => {
   const router = useRouter();
@@ -23,6 +24,13 @@ const CustomLink = ({ href, title, className="" }) => {
 }
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState()
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+    console.log(isOpen)
+  }
+
   return (
     <header className="absolute lg:inset-x-24 xl:inset-x-28 top-0 z-20">
       <nav className="flex items-center justify-between lg:px-8" aria-label="Global">
@@ -33,7 +41,8 @@ export default function Header() {
           </a>
         </div>
         <div className="flex lg:hidden">
-          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"  
+          onClick={handleClick}>
             <span className="sr-only">Open main menu</span>
             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -63,7 +72,7 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <div className="lg:hidden" role="dialog" aria-modal="true">
+      <div className={`${isOpen ? 'hidden' : ''} lg:hidden`} role="dialog" aria-modal="true">
         <div className="fixed inset-0 z-50"></div>
         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
@@ -71,7 +80,7 @@ export default function Header() {
               <span className="sr-only">Your Company</span>
               <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
             </a>
-            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={handleClick}>
               <span className="sr-only">Close menu</span>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
