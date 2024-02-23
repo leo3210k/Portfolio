1815b2781d56d6a20dbb5eddf414211fae4c0a16
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-22 21:41:11 -0300

    change size of menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,7 +44,7 @@ export default function Header() {
           <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"  
           onClick={handleClick}>
             <span className="sr-only">Open main menu</span>
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
