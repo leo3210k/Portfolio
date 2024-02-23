81d1c0c15e691a04678b45ea074bfe287e41882d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-22 22:59:28 -0300

    add margin-bottom on main content on medium and below

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ export default function Index() {
     <div className="text-center bg-white">
       <Header />
       <Main>
-        <div class="mx-auto flex flex-col md:flex-row sm:justify-center lg:justify-around items-center gap-y-7 mt-10 md:mt-80 lg:mt-48 xl:mt-24">
+        <div class="mx-auto flex flex-col md:flex-row sm:justify-center lg:justify-around items-center gap-y-7 mt-10 md:mt-80 lg:mt-48 xl:mt-24 mb-10 md:mb-0">
           <Image src={profile} className="w-9/12 md:hidden lg:block lg:w-4/12 h-auto" alt="profile"/>
           <div class="sm:w-11/12 lg:w-5/12 flex flex-col lg:items-start">
             <AnimatedText text="Turning Vision Into Reality With Code And Design."
