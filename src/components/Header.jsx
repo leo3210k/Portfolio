5156b4774d4df3a7d303f1e8ea9185e37f2fe2a6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-23 17:44:50 -0300

    fix menu button ternary operator to show

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const CustomLink = ({ href, title, className="" }) => {
 }
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState()
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleClick = () => {
     setIsOpen(!isOpen)
@@ -72,7 +72,7 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <div className={`${isOpen ? 'hidden' : ''} lg:hidden`} role="dialog" aria-modal="true">
+      <div className={`${isOpen ? '' : 'hidden'}`} role="dialog" aria-modal="true">
         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
