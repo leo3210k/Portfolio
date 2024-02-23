9a1fe89e1241d50dc74d7b493b14c8f09f3e7190
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-23 18:33:59 -0300

    change biography text size

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -20,17 +20,17 @@ export default function About() {
           className="text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-8xl"/>
           <div className="mt-20 flex justify-around items-center">
             <div className="w-4/12">
-              <h2 class="text-xl text-left font-bold leading-8 text-gray-600">BIOGRAPHY</h2>
-              <p class="mt-4 text-base text-left leading-6 text-gray-600">
+              <h2 class="text-3xl text-left font-bold leading-8 text-gray-600">BIOGRAPHY</h2>
+              <p class="mt-4 text-lg text-left leading-6 text-gray-600">
                 Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and 
                 innovative ways to bring my clients' visions to life.
               </p>
-              <p class="mt-4 text-base text-left leading-6 text-gray-600">
+              <p class="mt-4 text-lg text-left leading-6 text-gray-600">
                 I believe that design is about more than just making things look pretty – it's about solving problems and creating 
                 intuitive, enjoyable experiences for users.
               </p>
-              <p class="mt-4 text-base text-left leading-6 text-gray-600">
+              <p class="mt-4 text-lg text-left leading-6 text-gray-600">
                 Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence 
                 and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and 
                 passion to your next project.
