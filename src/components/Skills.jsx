718b5ad4be457b257e89d4d98b9fdcda76ab89a6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-24 01:08:58 -0300

    fix skills places

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -26,14 +26,14 @@ export default function Skills() {
         whileHover={{scale: 1.05}}>
           Web
         </motion.div>
-        <Skill name="HTML" x="-20vw" y="2vw"/>
-        <Skill name="CSS" x="-5vw" y="-10vw"/>
+        <Skill name="HTML" x="-32vw" y="2vw"/>
+        <Skill name="CSS" x="-5vw" y="-8vw"/>
         <Skill name="Javascript" x="20vw" y="6vw"/>
         <Skill name="ReactJS" x="0vw" y="12vw"/>
-        <Skill name="NextJS" x="-20vw" y="-15vw"/>
-        <Skill name="GatsbyJS" x="15vw" y="-12vw"/>
+        <Skill name="NextJS" x="-28vw" y="-15vw"/>
+        <Skill name="GatsbyJS" x="20vw" y="-14vw"/>
         <Skill name="Web Design" x="32vw" y="-5vw"/>
-        <Skill name="Figma" x="0vw" y="-20vw"/>
+        <Skill name="Figma" x="0vw" y="-21vw"/>
         <Skill name="Firebase" x="-25vw" y="18vw"/>
         <Skill name="Tailwind CSS" x="18vw" y="18vw"/>
       </div>
