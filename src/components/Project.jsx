98b98221de4bc7a8a8eae9bb5e9582c08c9a74c2
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-26 18:36:02 -0300

    add items center and h-screen to project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 export default function Main({ title, description, technologies, image_1, image_2, order=false }) {
   return (
-    <motion.div class="h-[80vh] lg:h-screen flex flex-col lg:flex-row justify-evenly"
+    <motion.div class="h-screen flex flex-col lg:flex-row justify-evenly items-center"
     initial={{ opacity: 0, y: 50 }}
     whileInView={{ opacity: 1, y: 0 }}
     transition={{ delay: 1, duration: 1 }}
