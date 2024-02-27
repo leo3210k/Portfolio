474673edfda69da5fa1476ed32faab8a947cc500
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-26 22:17:25 -0300

    add project title text size on medium extra small and below

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,7 +14,7 @@ export default function Main({ title, description, technologies, image_1, image_
         <Image src={`${image_2}`} width={200} height={200} className="absolute w-3/12 rounded-xl top-14 xs:top-20 sm:top-32 -right-6 xs:-right-10 shadow-2xl"/>
       </div>
       <div className={`lg:w-3/12 ${order ? 'lg:mr-32' : 'lg:ml-32'} flex flex-col justify-center items-center lg:items-baseline gap-y-1 text-left`}>
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900">{ title }</h1>
+        <h1 className="text-2xl xs:text-3xl md:text-4xl font-bold tracking-tight text-gray-900">{ title }</h1>
         <h2 className="font-medium text-gray-900">{ description }</h2>
         <span className="mt-2 italic font-normal text-gray-600">{ technologies }</span>
         <div className="mt-5 flex gap-x-6">
