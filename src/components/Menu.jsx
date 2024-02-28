c0abb622e3b754f23a227ca23923c2eb86ad7fbe
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 17:36:59 -0300

    add <li> tags to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,13 +5,13 @@ export default function Menu() {
     <nav className="menu bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul>
         <Link href={{ pathname: "/", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-          Home
+          <li>Home</li>
         </Link>
         <Link href={{ pathname: "/about", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-          About
+          <li>About</li>
         </Link>
         <Link href={{ pathname: "/projects", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-          Projects
+          <li>Projects</li>
         </Link>
       </ul>
     </nav> 
