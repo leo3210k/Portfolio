e457ad0a007aa5b73fe71c475002dcf006892102
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-27 23:33:08 -0300

    remove tailwind logo from menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,10 +77,6 @@ export default function Header() {
       <div className={`${isOpen ? '' : 'hidden'}`} role="dialog" aria-modal="true">
         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
-            </a>
             <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={handleClick}>
               <span className="sr-only">Close menu</span>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
