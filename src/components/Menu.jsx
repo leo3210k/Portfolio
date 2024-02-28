9cda5b1d44d664751bfc2b979473924f16d8e2bd
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 17:36:34 -0300

    add tailwind gradient to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Menu() {  
   return (
-    <nav className="menu">
+    <nav className="menu bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul>
         <Link href={{ pathname: "/", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
           Home
