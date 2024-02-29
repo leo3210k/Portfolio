8d1a616edbbf1251439360a8dfc5bbbff3dc0081
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 21:25:47 -0300

    add customlink to menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
+import CustomLink from "./CustomLink";
 
 export default function Menu() {  
   return (
     <nav className="menu bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul>
-        <Link href={{ pathname: "/", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+        <CustomLink href="/" title="Home" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
           <li>Home</li>
-        </Link>
-        <Link href={{ pathname: "/about", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+        </CustomLink>
+        <CustomLink href="/about" title="About" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
           <li>About</li>
-        </Link>
-        <Link href={{ pathname: "/projects", query: { name: '?addEffect=true' }}} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+        </CustomLink>
+        <CustomLink href="/projects" title="Projects" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
           <li>Projects</li>
-        </Link>
+        </CustomLink>
       </ul>
     </nav> 
   )
