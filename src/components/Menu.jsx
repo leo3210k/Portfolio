a0f3cdab8a8fe230a51d0088385074233e6488ae
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 22:29:45 -0300

    add self center to social medias on menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import { Github, Instagram, Linkedin, Pinterest, Twitter } from './Icons';
 import CustomLink from "./CustomLink";
 
 export default function Menu() {  
   return (
-    <nav className="menu bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
+    <nav className="menu flex flex-col bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul>
         <CustomLink href="/" title="Home" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
         <CustomLink href="/about" title="About" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
         <CustomLink href="/projects" title="Projects" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
       </ul>
-      <div className="hidden gap-x-8 lg:flex lg:justify-end flex-wrap">
+      <div className="flex self-center gap-x-8 flex-wrap">
           <Link href="/" target={"_blank"}>
             <Github className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
           </Link>
