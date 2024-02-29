9bdd9663814464ce5d13f125436b0ebb4075ba89
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 21:28:59 -0300

    remove customlink to create a file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,6 @@ import { MenuToggle } from "./MenuToggle";
 import Menu from './Menu'
 import animatedMenu from './animatedMenu'
 
-const CustomLink = ({ href, title, className="" }) => {
-  const router = useRouter();
-  return(
-    <Link href={{ pathname: href, query: { name: 'addEffect=true' }}} className={`${className} relative group`}>
-      {title}
-
-      <span className={`
-      h-[1px] inline-block bg-black
-      absolute left-0 -bottom-0.5
-      group-hover:w-full transition-[width] ease duration-300
-      underline-offset-4
-      ${router.asPath === href ? "w-full" : "w-0"}
-      `}
-      >&nbsp;</span>
-    </Link>
-  )
-}
-
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false)
 
