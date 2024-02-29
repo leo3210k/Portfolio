48de613b965d83911a7e70ba0f73c1175eca62f0
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 21:26:50 -0300

    remove customlink children

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,15 +5,9 @@ export default function Menu() {
   return (
     <nav className="menu bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul>
-        <CustomLink href="/" title="Home" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-          <li>Home</li>
-        </CustomLink>
-        <CustomLink href="/about" title="About" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-          <li>About</li>
-        </CustomLink>
-        <CustomLink href="/projects" title="Projects" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-          <li>Projects</li>
-        </CustomLink>
+        <CustomLink href="/" title="Home" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" />
+        <CustomLink href="/about" title="About" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" />
+        <CustomLink href="/projects" title="Projects" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" />
       </ul>
     </nav> 
   )
