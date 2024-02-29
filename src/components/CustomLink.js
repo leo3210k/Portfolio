fa2c96f7d67471d2bbb0adae2c2e742022a7aeea
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 21:24:45 -0300

    add span tag to li tag

--- src/components/CustomLink.js
+++ src/components/CustomLink.js
@@ -6,16 +6,18 @@ export default function CustomLink({ href, title, className="", background }) {
 
   return(
     <Link href={{ pathname: href, query: { name: 'addEffect=true' }}} className={`${className} relative group`}>
-      {title}
+      <li>
+        {title}
+        <span className={`
+        h-[1px] inline-block
+        absolute left-0 -bottom-0.5 bg-${background}
+        group-hover:w-full transition-[width] ease duration-300
+        underline-offset-4
+        ${router.asPath === href ? "w-full" : "w-0"}
+        `}
+        >&nbsp;</span>
+      </li>
 
-      <span className={`
-      h-[1px] inline-block
-      absolute left-0 -bottom-0.5 bg-${background}
-      group-hover:w-full transition-[width] ease duration-300
-      underline-offset-4
-      ${router.asPath === href ? "w-full" : "w-0"}
-      `}
-      >&nbsp;</span>
     </Link>
   )
 }
