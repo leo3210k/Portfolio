b30a5d956e84753ba8007092d5cf6344f861aaed
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 22:29:11 -0300

    add social medias icons to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,6 +9,23 @@ export default function Menu() {
         <CustomLink href="/about" title="About" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
         <CustomLink href="/projects" title="Projects" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
       </ul>
+      <div className="hidden gap-x-8 lg:flex lg:justify-end flex-wrap">
+          <Link href="/" target={"_blank"}>
+            <Github className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
+          </Link>
+          <Link href="/" target={"_blank"}>
+            <Linkedin className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
+          </Link>
+          <Link href="/" target={"_blank"}>
+            <Instagram className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
+          </Link>
+          <Link href="/" target={"_blank"}>
+            <Twitter className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
+          </Link>
+          <Link href="/" target={"_blank"}>
+            <Pinterest className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
+          </Link>
+        </div>
     </nav> 
   )
 }
