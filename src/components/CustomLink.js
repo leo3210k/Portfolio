9c783511992ddee684afb0bd3a33994c08e233a1
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 21:24:11 -0300

    add background parameter

--- src/components/CustomLink.js
+++ src/components/CustomLink.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function CustomLink({ href, title, className="" }) {
+export default function CustomLink({ href, title, className="", background }) {
   const router = useRouter();
+
   return(
     <Link href={{ pathname: href, query: { name: 'addEffect=true' }}} className={`${className} relative group`}>
       {title}
 
       <span className={`
-      h-[1px] inline-block bg-black
-      absolute left-0 -bottom-0.5
+      h-[1px] inline-block
+      absolute left-0 -bottom-0.5 bg-${background}
       group-hover:w-full transition-[width] ease duration-300
       underline-offset-4
       ${router.asPath === href ? "w-full" : "w-0"}
