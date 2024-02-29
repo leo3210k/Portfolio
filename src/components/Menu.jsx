9cf88347555f08c0c509e7308144103caceeeae4
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-28 21:27:48 -0300

    remove background hover

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,9 +5,9 @@ export default function Menu() {
   return (
     <nav className="menu bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul>
-        <CustomLink href="/" title="Home" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50" background="white" />
-        <CustomLink href="/about" title="About" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50" background="white" />
-        <CustomLink href="/projects" title="Projects" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50" background="white" />
+        <CustomLink href="/" title="Home" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
+        <CustomLink href="/about" title="About" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
+        <CustomLink href="/projects" title="Projects" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
       </ul>
     </nav> 
   )
