ac96b059e68e60c3f3add9c1422522a87ece2341
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-29 21:20:49 -0300

    add space and margin between links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,10 +5,10 @@ import CustomLink from "./CustomLink";
 export default function Menu() {  
   return (
     <nav className="menu flex flex-col bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
-      <ul>
-        <CustomLink href="/" title="Home" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
-        <CustomLink href="/about" title="About" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
-        <CustomLink href="/projects" title="Projects" className="-mx-3 block px-3 py-2 text-base font-semibold leading-7 text-white" background="white" />
+      <ul className="space-y-2">
+        <CustomLink href="/" title="Home" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 text-black bg-white p-2 rounded-xl" background="black" />
+        <CustomLink href="/about" title="About" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 text-black bg-white p-2 rounded-xl" background="black" />
+        <CustomLink href="/projects" title="Projects" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 text-black bg-white p-2 rounded-xl" background="black" />
       </ul>
       <div className="flex self-center gap-x-8 flex-wrap">
           <Link href="/" className="p-2 rounded-xl bg-white" target={"_blank"}>
