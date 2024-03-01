3f836d02826ec73d181c4fc89de6a5eb06d7dd5b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-02-29 21:21:32 -0300

    add text gradient

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,9 +6,9 @@ export default function Menu() {
   return (
     <nav className="menu flex flex-col bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]">
       <ul className="space-y-2">
-        <CustomLink href="/" title="Home" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 text-black bg-white p-2 rounded-xl" background="black" />
-        <CustomLink href="/about" title="About" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 text-black bg-white p-2 rounded-xl" background="black" />
-        <CustomLink href="/projects" title="Projects" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 text-black bg-white p-2 rounded-xl" background="black" />
+        <CustomLink href="/" title="Home" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 p-2 bg-white rounded-xl" text_color="text-transparent bg-clip-text bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]" background="black" />
+        <CustomLink href="/about" title="About" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 p-2 bg-white rounded-xl" text_color="text-transparent bg-clip-text bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]" background="black" />
+        <CustomLink href="/projects" title="Projects" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 p-2 bg-white rounded-xl" text_color="text-transparent bg-clip-text bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]" background="black" />
       </ul>
       <div className="flex self-center gap-x-8 flex-wrap">
           <Link href="/" className="p-2 rounded-xl bg-white" target={"_blank"}>
