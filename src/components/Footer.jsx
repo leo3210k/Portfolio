ff67110b5a130e2c7eec58ed960aac2865375038
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-04 22:57:40 -0300

    translate footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer({ children }) {
   return (
     <footer className="p-10 flex justify-around content-center border-t-2 border-gray-900">
       <span className="flex">
-        Built with Nextjs and &nbsp;&nbsp;
+        Construído com Nextjs e &nbsp;&nbsp;
         <Heart className="w-6 h-auto text-red-700 animate-ping-slow" alt="logo"/>
       </span>
     </footer>
