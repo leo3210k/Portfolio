22f9aed7f3f74946ee3037d3ebd95b2610b163f2
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-13 20:29:19 -0300

    add alt to images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,8 +11,8 @@ export default function Main({ title, description, technologies, image_1, image_
     transition={{ delay: 1, duration: 1 }}
     viewport={{ once: true }}>
       <div className={`relative self-center ${order ? 'lg:order-last' : ''}`}>
-        <Image src={`${image_1}`} width={800} height={800} className="rounded-xl shadow-2xl"/>
-        <Image src={`${image_2}`} width={200} height={200} className="absolute w-3/12 rounded-xl top-14 xs:top-20 sm:top-32 -right-6 xs:-right-10 shadow-2xl"/>
+        <Image src={`${image_1}`} width={800} height={800} className="rounded-xl shadow-2xl" alt="project-desktop"/>
+        <Image src={`${image_2}`} width={200} height={200} className="absolute w-3/12 rounded-xl top-14 xs:top-20 sm:top-32 -right-6 xs:-right-10 shadow-2xl" alt="project-mobile"/>
       </div>
       <div className={`lg:w-3/12 ${order ? 'lg:mr-32' : 'lg:ml-32'} flex flex-col justify-center items-center lg:items-baseline gap-y-1 text-left`}>
         <h1 className="text-2xl xs:text-3xl md:text-4xl font-bold tracking-tight text-gray-900">{ title }</h1>
