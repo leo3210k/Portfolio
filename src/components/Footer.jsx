727be7d0e5b7b089f3933b3db85095a27b67fbe9
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-14 17:04:19 -0300

    add justify evenly to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Heart } from "./utils/Icons";
 
 export default function Footer({ children }) {
   return (
-    <footer className="px-10 py-6 flex justify-around content-center border-t-2 border-gray-900">
+    <footer className="px-10 py-6 flex justify-evenly items-center border-t-2 border-gray-900">
       <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
       <span className="flex">
         Construído com Nextjs e &nbsp;&nbsp;
