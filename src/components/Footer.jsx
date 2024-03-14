e5263b22f7635945a0ad8e4c9e15e77f9c8b48e0
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-14 16:50:30 -0300

    change footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Heart } from "./utils/Icons";
 
 export default function Footer({ children }) {
   return (
-    <footer className="p-10 flex justify-around content-center border-t-2 border-gray-900">
+    <footer className="px-10 py-6 flex justify-around content-center border-t-2 border-gray-900">
       <span className="flex">
         Construído com Nextjs e &nbsp;&nbsp;
         <Heart className="w-6 h-auto text-red-700 animate-ping-slow" alt="logo"/>
