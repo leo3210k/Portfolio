5b8ec75691191693ffadaf774c5bb06d8a80a114
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-14 17:03:46 -0300

    add copyright to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { Heart } from "./utils/Icons";
 export default function Footer({ children }) {
   return (
     <footer className="px-10 py-6 flex justify-around content-center border-t-2 border-gray-900">
+      <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
       <span className="flex">
         Construído com Nextjs e &nbsp;&nbsp;
         <Heart className="w-6 h-auto text-red-700 animate-ping-slow" alt="logo"/>
