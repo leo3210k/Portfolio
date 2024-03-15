bb0a0eec36500df8e3f3d4b87ea901bed5579f0b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-14 21:39:31 -0300

    add urls to menu icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,13 +11,13 @@ export default function Menu() {
         <CustomLink href="/projects" title="Projetos" className="mx-4 block px-3 py-2 text-base font-semibold leading-7 p-2 bg-white rounded-xl" text_color="text-transparent bg-clip-text bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]" background="gradient-to-tr from-[#ff80b5] to-[#9089fc]" />
       </ul>
       <div className="mx-4 flex justify-around self-center gap-x-8 gap-y-4 flex-wrap">
-          <Link href="/" className="p-2 rounded-xl bg-white" target={"_blank"}>
+          <Link href="https://github.com/leo3210k" className="p-2 rounded-xl bg-white" target={"_blank"}>
             <Github className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
           </Link>
-          <Link href="/" className="p-2 rounded-xl bg-white" target={"_blank"}>
+          <Link href="https://www.linkedin.com/in/leonardo-coêlho-9599b9200" className="p-2 rounded-xl bg-white" target={"_blank"}>
             <Linkedin className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
           </Link>
-          <Link href="/" className="p-2 rounded-xl bg-white" target={"_blank"}>
+          <Link href="https://www.instagram.com/leocoelho.pi/" className="p-2 rounded-xl bg-white" target={"_blank"}>
             <Instagram_2 className="w-6 h-auto hover:scale-125 transition duration-200" alt="logo"/>
           </Link>
         </div>
