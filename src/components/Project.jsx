65bd3d8441963ab608e29d4dbb726ca5abc622ee
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-03-14 21:32:22 -0300

    add margin top on landscape

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,7 +14,7 @@ export default function Main({ title, description, technologies, image_1, image_
         <Image src={`${image_1}`} width={800} height={800} className="rounded-xl shadow-2xl" alt="project-desktop"/>
         <Image src={`${image_2}`} width={200} height={200} className="absolute w-3/12 rounded-xl top-14 xs:top-20 sm:top-32 -right-6 xs:-right-10 shadow-2xl" alt="project-mobile"/>
       </div>
-      <div className={`lg:w-3/12 ${order ? 'lg:mr-32' : 'lg:ml-32'} flex flex-col justify-center items-center lg:items-baseline gap-y-1 text-left`}>
+      <div className={`lg:w-3/12 ${order ? 'lg:mr-32' : 'lg:ml-32'} flex flex-col justify-center items-center lg:items-baseline gap-y-1 text-left landscape:mt-10`}>
         <h1 className="text-2xl xs:text-3xl md:text-4xl font-bold tracking-tight text-gray-900">{ title }</h1>
         <h2 className="text-center md:text-justify font-medium text-gray-900">{ description }</h2>
         <span className="mt-2 italic text-center md:text-justify font-normal text-gray-600">{ technologies }</span>
